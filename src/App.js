d4046177804f03fe5b22419d0adb736c045f2dbb
Grégoire Hertault <ghertault@gmail.com>
2020-06-26 10:27:54 +0200

    Install new deps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import { Amplify } from "@aws-amplify/core";
 import React from "react";
 import { Resource } from "react-admin";
-import { AmplifyAdmin, configureAmplify } from "react-admin-amplify";
-import config from "./aws-exports";
+import { AmplifyAdmin } from "react-admin-amplify";
+import awsExports from "./aws-exports";
+import { Dashboard } from "./components/Dashboard";
 import { OrderList } from "./components/Order";
 import * as mutations from "./graphql/mutations";
 import * as queries from "./graphql/queries";
 
-configureAmplify(config);
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <AmplifyAdmin
       operations={{ queries, mutations }}
       options={{ adminGroups: ["admin", "editor"] }}
+      dashboard={Dashboard}
     >
       <Resource name="orders" list={OrderList} />
     </AmplifyAdmin>
---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import * as React from "react";
+import { Title } from "react-admin";
+
+export const Dashboard = () => (
+  <Card>
+    <Title title="React Admin Amplify Demo" />
+    <CardContent>Welcome to the demo!</CardContent>
+  </Card>
+);
