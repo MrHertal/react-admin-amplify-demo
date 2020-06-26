e8ae6b38bbae52fda682ebc74720d1f4989669b3
Grégoire Hertault <ghertault@gmail.com>
2020-06-26 17:14:21 +0200

    Complete order resource and build a pagination (#2)

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import {
+  Create,
   Datagrid,
   DateField,
   DateInput,
+  Edit,
+  EditButton,
   List,
   NumberField,
+  NumberInput,
+  Show,
+  ShowButton,
+  SimpleForm,
+  SimpleShowLayout,
   TextField,
   TextInput,
 } from "react-admin";
 import { AmplifyFilter } from "react-admin-amplify";
+import { AmplifyPagination } from "./AmplifyPagination";
 
 const defaultQuery = "listOrders";
 
@@ -45,7 +54,11 @@ export const OrderList = (props) => {
   const [query, setQuery] = useState(defaultQuery);
 
   return (
-    <List {...props} filters={<OrderFilter setQuery={setQuery} />}>
+    <List
+      {...props}
+      filters={<OrderFilter setQuery={setQuery} />}
+      pagination={<AmplifyPagination />}
+    >
       <Datagrid>
         <TextField
           source="id"
@@ -71,7 +84,61 @@ export const OrderList = (props) => {
         />
         <DateField source="createdAt" sortable={false} />
         <DateField source="updatedAt" sortable={false} />
+        <ShowButton />
+        <EditButton />
       </Datagrid>
     </List>
   );
 };
+
+export const OrderShow = (props) => (
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="customerID" label="Customer" />
+      <TextField
+        source="accountRepresentativeID"
+        label="Account representative"
+      />
+      <TextField source="productID" label="Product" />
+      <TextField source="status" />
+      <NumberField source="amount" />
+      <DateField source="date" />
+      <DateField source="createdAt" />
+      <DateField source="updatedAt" />
+    </SimpleShowLayout>
+  </Show>
+);
+
+export const OrderEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput source="id" disabled />
+      <TextInput source="customerID" label="Customer" />
+      <TextInput
+        source="accountRepresentativeID"
+        label="Account representative"
+      />
+      <TextInput source="productID" label="Product" />
+      <TextInput source="status" />
+      <NumberInput source="amount" />
+      <DateInput source="date" />
+    </SimpleForm>
+  </Edit>
+);
+
+export const OrderCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="customerID" label="Customer" />
+      <TextInput
+        source="accountRepresentativeID"
+        label="Account representative"
+      />
+      <TextInput source="productID" label="Product" />
+      <TextInput source="status" />
+      <NumberInput source="amount" />
+      <DateInput source="date" />
+    </SimpleForm>
+  </Create>
+);
