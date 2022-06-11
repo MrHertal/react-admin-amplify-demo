236d75da0cf0f37b2b3e7ed3da285366432c342b
Grégoire Hertault <ghertault@gmail.com>
2022-06-11 15:36:51 +0200

    Update all dependencies

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,41 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactadminamplifydem51487c4e51487c4e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        },
+        "userPoolGroups": {
+            "superadminGroupRole": "string",
+            "adminGroupRole": "string"
+        }
+    },
+    "api": {
+        "reactadminamplifydem": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        },
+        "AdminQueries": {
+            "RootUrl": "string",
+            "ApiName": "string"
+        }
+    },
+    "storage": {
+        "s360f76f8b": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "AdminQueries6ffead5e": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
